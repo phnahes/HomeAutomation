c3379129264219b2109259bda9b2b50a0d28b0de
Paulo Nahes <phnahes@gmail.com>
2013-03-16 14:36:08 -0300

    Added resources to server automation using knife-solo and chef-solo

---
+++ Automacao/chef/cookbooks/sudo/recipes/default.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: sudo
+# Recipe:: default
+#
+# Copyright 2008-2009, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+case node[:platform]
+  when "debian", "ubuntu"
+    execute "bash -c 'export SUDO_FORCE_REMOVE=yes'"
+
+#    package "sudo-ldap" do
+#      action :upgrade
+#    end
+  else
+    package "sudo" do
+      action :upgrade
+    end
+end
+
+template "/etc/sudoers" do
+  source "sudoers.erb"
+  mode "0440"
+  owner "root"
+  group "root"
+  variables(
+    :sudoers_groups => node[:authorization][:sudo][:groups], 
+    :sudoers_users => node[:authorization][:sudo][:users]
+  )
+end
